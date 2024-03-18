ca906f1e7004aefe3062d0856a61b1a86c9b418d
Anna Dudek <75646880+Anna-portfolio@users.noreply.github.com>
2024-03-18 01:48:34 +0100

    Update burger.js

--- burger.js
+++ burger.js
@@ -5,6 +5,7 @@ function burgerMenu() {
         navMenu.classList.toggle("nav-menu-small-active");
         navMenu.classList.add("nav-menu-small");
         burger.classList.toggle("toX");
+
 }
 navMenu.classList.remove("nav-menu-small");
 burger.classList.remove("toX");
